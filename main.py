4aad32df761d294133d78e934edcb7a40cc31155
riebz <151185012+riebz@users.noreply.github.com>
2024-04-26 18:35:03 +0300

    final

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
+import requests, os
 from flask import Flask, render_template, request, redirect, url_for, session
 from game_X_O import create_board, check_winner, is_board_full, make_move
 from game_9_9 import create_big_board, make_move_9_9, check_big_winner, is_big_board_full
 
 app = Flask(__name__)
-app.secret_key = 'very_secret_key'
+app.secret_key = '3d5cfb7d140b469535655b3dca1ce307'
+
+
+app = Flask(__name__)
+app.secret_key = '3d5cfb7d140b469535655b3dca1ce307'
+
+API_KEY = '3d5cfb7d140b469535655b3dca1ce307'
+
+API_KEY = os.getenv('3d5cfb7d140b469535655b3dca1ce307')
+
+@app.route('/weather', methods=['GET'])
+def weather():
+    city = 'Moscow'
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather_data = response.json()
+        return render_template('weather.html', weather_data=weather_data)
+    else:
+        error_message = response.json().get('message', 'Error getting weather data')
+        return f"Error getting weather data: {error_message}", response.status_code
+
+users = {}
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username and username not in users:
+            users[username] = {'username': username}
+            session['username'] = username
+            return redirect(url_for('index'))  # Перенаправление на главную страницу после регистрации
+        else:
+            error = "Это имя пользователя уже занято или некорректно."
+            return render_template('register.html', error=error)
+    return render_template('register.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -27,7 +63,7 @@ def game():
 
     if request.method == 'POST':
         if 'new_game' in request.form:
-            return redirect(url_for('/'))
+            return redirect(url_for('index'))
 
         move = request.form.get('move')
         if move:
